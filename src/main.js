c55b0dc1eac4c2ce606e12e73d625dd22a5ae557
theyokky <Yanakorn.j@ku.th>
2021-08-08 11:19:50 +0700

    added vue-youtube

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueYoutube from 'vue-youtube'
+
+Vue.use(VueYoutube)
 
 Vue.config.productionTip = false
 
